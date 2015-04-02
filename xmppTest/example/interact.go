2f874dd1522f99236c6c802bbcaff8aaac6dec1f
Robert Morewood <RMorewood@oLc.ubc.ca>
2015-04-02 13:40:00 -0700

    Stable logins and cleanup up messaging

--- xmppTest/example/interact.go
+++ xmppTest/example/interact.go
@@ -4,13 +4,13 @@ import (
 	"../xmpp"
 	"crypto/tls"
 	cryptorand "crypto/rand"
-	"encoding/xml"
+	//"encoding/xml"
 	"flag"
 	"fmt"
 	"math/rand"
 	"os"
 	"strconv"
-	"strings"
+//	"strings"
 	"time"
 	"runtime"
 	"sync"
@@ -20,85 +20,112 @@ type XmppUser struct {
 	Client *xmpp.Client
 	Status xmpp.Status
 	Time int64
+	Retry int
 }
 
-// Demonstrate the API, and allow the user to interact with an XMPP
-// server via the terminal.
+const (
+	XMPP_SERVER = "aziz.xmpp.vidao.com"
+	USER_PREFIX = "vidao"
+	DEFAULT_USER_COUNT = 100
+	LOGIN_WORKERS = 35
+	MESSAGE_DELAY = 10000000 // nanoseconds
+)
+
+// Stress test an XMPP server.
+// with predefined users: <USER_PREFIX><COUNT>
+// where count starts from "1".
+// and password is username.
+
 func main() {
 	runtime.GOMAXPROCS(60)
 
 	fmt.Println("Starting client")
-	numberOfUsers := flag.Int("numb", 42, "Number of user")
+	userCount := flag.Int("numb", DEFAULT_USER_COUNT, "Number of users")
 	flag.Parse()
-	inChannel := generateXMPPusers(*numberOfUsers)
+	inChannel := generateXMPPusers(*userCount)
 
 	// Create a return channel for each worker
-	workers := 10
 	var logins sync.WaitGroup
-	logins.Add(workers)
-	userChannel := make(chan *XmppUser, *numberOfUsers)
+	logins.Add(LOGIN_WORKERS)
+	userChannel := make(chan *XmppUser, *userCount)
 	Messages := make(chan xmpp.Stanza)
 	startRequestTimer := time.Now().Unix()
-	for worker := 0; worker < workers; worker++ {
+	for worker := 0; worker < LOGIN_WORKERS; worker++ {
 		go loginWorker(inChannel, worker, Messages, userChannel, &logins) 
 	}
 	logins.Wait()
 	endRequestTimer := time.Now().Unix()
 	close(userChannel)
-	var xmppUsers []*XmppUser
-	for xmpp := range userChannel {
-		fmt.Printf("Adding Client %s\n",xmpp.Client.Jid)
-		xmppUsers = append(xmppUsers, xmpp)
-	}
-	
+
 	// Statistics follow.
 	numOfClients := 0
 	totalLoginTime := int64(0)
 	totalTime := int64(0)
-	for counter := 0; counter < len(xmppUsers); counter++ {
+	retries := 0
+	retryTotal := 0
+	var xmppUsers []*XmppUser
+	for xmpp := range userChannel {
+		xmppUsers = append(xmppUsers, xmpp)
 		fmt.Printf("status=%d login time=%d %s\n", 
-			xmppUsers[counter].Status, xmppUsers[counter].Time, 
-			xmppUsers[counter].Client.Jid);
-		if xmppUsers[counter].Status == 5 {
+			xmpp.Status, xmpp.Time, xmpp.Client.Jid);
+		if xmpp.Status == 5 {
 			numOfClients = numOfClients + 1
-			//fmt.Printf("Jid :%v  and  Status :%v \n", xmppUsers[counter].Client.Jid, xmppUsers[counter].Status)
-			totalLoginTime += xmppUsers[counter].Time
+			totalLoginTime += xmpp.Time
 		}
-		totalTime += xmppUsers[counter].Time
-		defer xmppUsers[counter].Client.Close()
-
+		if xmpp.Retry>1 { 
+			retries++
+			retryTotal += xmpp.Retry
+		}
+		totalTime += xmpp.Time
+		Client := xmpp.Client
+		defer Client.Close()		
 	}
 	fmt.Printf(
-		"\n\n\n\nThe clients (Total : %d of %d) which succeed (sending rate - %f per second):\n", 
-		len(xmppUsers), *numberOfUsers,
-		float32(*numberOfUsers)/float32(endRequestTimer - startRequestTimer))
+		"\n\n\n\nThe clients (Total : %d of %d) which succeed (sending rate - %.2f per second):\n", 
+		len(xmppUsers), *userCount,
+		float32(*userCount)/float32(endRequestTimer - startRequestTimer))
+	if retries>0 {
+		fmt.Printf( "%d connections had to retry with an average of %.2f failed logins \n",
+			retries,float32(retryTotal)/float32(retries)-1)
+	}
 	if numOfClients > 0 {
 		fmt.Printf(
-			"\n\n\nSuccessfully logged in users : %d out of %d \n"+
-			"Average Succesful NewClient Time: %f\n"+
-			"Total Average NewClientTime %f seconds\n", 
+			"\nSuccessfully logged in users : %d out of %d \n"+
+			"Average Succesful NewClient Time: %.2f\n"+
+			"Total Average NewClientTime %.2f seconds\n", 
 			numOfClients,len(xmppUsers),
 			float32(totalLoginTime)/float32(numOfClients)/1000000000, 
 			float32(totalTime)/float32(len(xmppUsers))/1000000000)
-	} else {
-		fmt.Printf("\n\n\n No clients successfully logged in. Average Time: %d\n",
+	} else if len(xmppUsers)>0 {
+		fmt.Printf("\n\n\nNo clients successfully logged in. Average Time: %d\n",
 			totalTime/int64(len(xmppUsers)))
+	} else {
+		fmt.Println("\n\n\nNo viable clients returned in total time %d\n",totalTime)
 	}
 	buffer := make([]byte, 1)
+	fmt.Println("\nPaused.  Press the \"Enter\" key to continue.\n(Press enter again to finish.)")
 	os.Stdin.Read(buffer)  // pause
 	
 	//  Message Sending Part
 	MessageSent := make(map[string]time.Time, 0)
+	
 	messagesReceived := 0
 	messagesSent := 0
 	timeouts := 0
 	averageTransmitTime := int64(0)
 	longestWait := int64(0)
+
 	startTime := time.Now()
-	delay := time.Duration(100000)
-	nextSend := startTime.Add(delay)
-	for {
-	select {
+	nextSend := startTime.Add(time.Duration(MESSAGE_DELAY))
+
+	working := true
+	go func() {
+		os.Stdin.Read(buffer) // pause
+		working = false
+	}()
+
+	for working { 
+		select {
 		case message := <-Messages :
 			//fmt.Printf("Incoming parsed (client# %v)XML:\n%#v\n\n", thisClient.Jid, object)
 			///===============================================================================================
@@ -118,13 +145,13 @@ func main() {
 				delete(MessageSent,messageID)
 				fmt.Printf("Sent %d - per second %f\n",messagesSent,
 					float32(messagesSent)*1000000000/float32(ReceivedTime.Sub(startTime)))
-				fmt.Printf("Back %d (timeouts %f%%) - ave transmit time %d (longest %d)\n",
+				fmt.Printf("Back %d (timeouts %.2f%%) - ave transmit time %d (longest %d)\n",
 					messagesReceived,float32(100*timeouts)/float32(messagesSent),
 					averageTransmitTime,longestWait)
 			}
 		default:
 			if time.Now().After(nextSend) {
-				nextSend = nextSend.Add(delay)
+				nextSend = nextSend.Add(time.Duration(MESSAGE_DELAY))
 				sIndex := rand.Intn(len(xmppUsers))
 				for xmppUsers[sIndex].Status !=5 {
 					sIndex = rand.Intn(len(xmppUsers))
@@ -133,25 +160,28 @@ func main() {
 				for rIndex == sIndex || xmppUsers[rIndex].Status != 5 {
 					rIndex = rand.Intn(len(xmppUsers))
 				}
-				sender := strings.Split(string(xmppUsers[sIndex].Client.Jid), "/")[0]
-				receiver := strings.Split(string(xmppUsers[rIndex].Client.Jid), "/")[0]
+				sender := xmppUsers[sIndex].Client.Jid
+				receiver := xmppUsers[rIndex].Client.Jid
 				
-				message := "<message id='" + strconv.Itoa(messagesSent) + "' from='" + sender + "' to='" + receiver + "' xml:lang='en' xmlns='jabber:client'><body>" + rand_str(5) + "</body></message>"
-
 				stanza := &xmpp.Message{}
-				xmlDecoder := xml.NewDecoder(strings.NewReader(message))
-				err := xmlDecoder.Decode(stanza)
-				if err == nil {
-					//fmt.Printf("Sending:\n%#v\n\n", stanza)
-					xmppUsers[sIndex].Client.Send <- stanza
-					MessageSent[stanza.GetHeader().Id] = time.Now()
-					messagesSent++
-				} else {
-					fmt.Printf("Parse error: %v\n\n", err)
-				}
-			}
-	}}
+				stanza.Header.Id = strconv.Itoa(messagesSent)
+				stanza.Header.From = sender
+				stanza.Header.To = receiver
+				stanza.Header.Lang = "en"
+				stanza.Body = []xmpp.Text{xmpp.Text{Chardata:rand_str(5)}}
 
+				xmppUsers[sIndex].Client.Send <- stanza
+				MessageSent[stanza.GetHeader().Id] = time.Now()
+				messagesSent++
+			}
+		}
+	}
+	fmt.Printf("\n\n\nComplete. Message Send Statistics:\n\n")
+	fmt.Printf("Sent %d - per second %f\n",messagesSent,
+		float32(messagesSent)*1000000000/float32(time.Now().Sub(startTime)))
+	fmt.Printf("Back %d (timeouts %.2f%%) - ave transmit time %d (longest %d)\n",
+		messagesReceived,float32(100*timeouts)/float32(messagesSent),
+		averageTransmitTime,longestWait)
 }
 
 func generateXMPPusers(numOfUsers int) chan int {
@@ -166,37 +196,57 @@ func generateXMPPusers(numOfUsers int) chan int {
 func loginWorker(in <-chan int, workerNum int, Messages chan xmpp.Stanza, 
 				out chan *XmppUser, logins *sync.WaitGroup) {
 	for counter := range in {
-		var tempXmppUser XmppUser
+		var newXmppUser XmppUser
 		user := strconv.Itoa(counter)
-		password := "vidao" + user
-		jidStr := "vidao" + user + "@aziz.xmpp.vidao.com"
+		password := USER_PREFIX + user
+		jidStr := USER_PREFIX + user + "@"+ XMPP_SERVER
 		fmt.Printf("Worker%d User : %s  Password : %s\n", workerNum, jidStr, password)
 		jid := xmpp.JID(jidStr)
 		// xmpp.JID is a string of the form <node>@<domain>/<resource>, with /<resource> optional.
-
+		done:=false
 		// Set up client connection section.
 		start:=time.Now()
-		for attempt:=0; attempt<5; attempt++ {
+		for !done && newXmppUser.Retry<5 {
 			status_updater := make(chan xmpp.Status)
-			go func(worker int, user string, tempXmppUser *XmppUser) {
+			thisLoginDone := make(chan bool)
+			go func(worker int, user string, newXmppUser *XmppUser) {
 				for status := range status_updater {
 					fmt.Printf("connection status(%s): %s\n", 
 						user, xmpp.StatusMessage[status])
-					tempXmppUser.Status = status
+					newXmppUser.Status = status
+					if status == 5 {
+						thisLoginDone <- true
+						close(thisLoginDone)
+					}
 				}
-			}(workerNum,user,&tempXmppUser)
+			}(workerNum,user,&newXmppUser)
 			tlsConf := tls.Config{InsecureSkipVerify: true}
 			client, err := xmpp.NewClient(&jid, password, &tlsConf, nil, 
 					xmpp.Presence{}, status_updater, Messages)
-			tempXmppUser.Time = int64(time.Now().Sub(start))
-			tempXmppUser.Client = client
-			fmt.Printf("%d) NewClient(%s) Status=%d\n", workerNum, user, tempXmppUser.Status)
+			newXmppUser.Time = int64(time.Now().Sub(start))
+			newXmppUser.Client = client
+			fmt.Printf("%d) NewClient(%s) Status=%d\n", workerNum, user, newXmppUser.Status)
 			if err != nil {
-				fmt.Printf("NewClientERROR(%v): %v\n", jid, err)
+				fmt.Printf("%d) NewClientERROR(%d:status=%d): %v\n", workerNum, user, newXmppUser.Status, err)
 			} else {
-				out <- &tempXmppUser
-				break
+				timeout := make(chan bool)
+				go func() {
+					time.Sleep(2*time.Second)
+					timeout <- true
+				}()
+				select {
+					case <-thisLoginDone :
+						out <- &newXmppUser
+						done = true
+					case <-timeout :
+						fmt.Printf("%d) NewClientERROR(%d:status=%d): Timed out waiting for running status.\n", 
+							workerNum, user, newXmppUser.Status, err)
+				}
 			}
+			newXmppUser.Retry++
+		}
+		if newXmppUser.Retry>=5 {
+			fmt.Printf("%d) Unable to login user %d after %d tries.\n", workerNum, user, newXmppUser.Retry)
 		}
 	}
 	logins.Done()
